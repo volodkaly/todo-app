df8a302bfd8f97ae8cccbab8809422d5ba2d1b43
volodkaly <lysvl@hotmail.cz>
2023-07-03 18:42:46 +0200

    bootstrap styles added and successfully applied to the main table

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,41 +7,36 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-<h1>Vítejte v aplikaci TODO-app. Zde můžete tvořit, upravovat a mazat úkoly.</h1>
-<h1>Seznam úkolů</h1>
-
+<h1 class="h1">Vítejte v aplikaci TODO-app. Zde můžete tvořit, upravovat a mazat úkoly.</h1>
 <table class="table table-striped table-hover">
     <thead>
     <tr>
         <th>
             <a href="{{ route('tasks.index', ['sort' => 'title']) }}">
                 <button type="button" class="btn btn-secondary">
-                Seřadit dle názvů
+                    Seřadit dle názvů
                 </button>
             </a>
         </th>
         <th>
-
         </th>
         <th>
             <a href="{{ route('tasks.index', ['sort' => 'deadline']) }}">
                 <button type="button" class="btn btn-secondary">
-                Seřadit dle termínů
+                    Seřadit dle termínů
                 </button>
             </a>
         </th>
         <th>
-
         </th>
         <th>
             <a href="{{ route('tasks.index', ['sort' => 'completed']) }}">
                 <button type="button" class="btn btn-secondary">
-                Seřadit dle stavu
-                    </button>
+                    Seřadit dle stavu
+                </button>
             </a>
         </th>
     </tr>
-
     <tr class="subhead">
         <th>Název</th>
         <th>Popis</th>
@@ -56,38 +51,47 @@
             <td>{{ $task->title }}</td>
             <td>{{ $task->content }}</td>
             <td>{{ date('d.m.Y', strtotime($task->deadline)) }}</td>
-            <td>
-                <a href="{{ route('tasks.edit', $task) }}">Upravit</a>
+            <td class="options">
 
-                <form method="post" action="{{ route('tasks.destroy', $task) }}" style="display:inline">
+                <a href="{{ route('tasks.edit', $task) }}">
+                    <button type="submit" class="btn btn-secondary m-2">
+                        Upravit
+                    </button>
+                </a>
+
+                <form method="post" action="{{ route('tasks.destroy', $task) }}">
                     @csrf
                     @method('DELETE')
-                    <button type="submit">Smazat</button>
+                    <button type="submit" class="btn btn-secondary m-2">
+                        Smazat
+                    </button>
                 </form>
+
             </td>
             <td>
                 <form method="POST" action="{{ route('tasks.status.update', $task->id) }}">
                     @method('PUT')
                     @csrf
                     <label>Nesplněno</label>
-                    <input type="range" min="0" max="1" name="completed" value="{{$task->completed}}">
+                    <input type="range" min="0" max="1" name="completed" value="{{$task->completed}}" >
                     <label>Splněno</label>
-
-                    <button type="submit">Uložit</button>
+                    <button type="submit" class="btn btn-secondary">Uložit</button>
                 </form>
             </td>
         </tr>
     @endforeach
     </tbody>
 </table>
-
-<a href="{{ route('tasks.create') }}">Nový úkol</a>
-
+<br>
+<a href="{{ route('tasks.create') }}">
+    <button type="submit" class="btn btn-secondary m-2">
+    Nový úkol
+    </button>
+</a>
 @if (session('error'))
     <div class="alert alert-danger">
         {{ session('error') }}
     </div>
 @endif
-
 </body>
 </html>
